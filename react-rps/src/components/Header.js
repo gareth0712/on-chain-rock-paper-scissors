e4f9f5ba38ae931b8d007cf3a03956e44512178e
gareth0712 <gareth_0712@hotmail.com>
2021-03-03 00:19:16 +0800

    Update Header to show available bankroll and fomo pool balance

--- react-rps/src/components/Header.js
+++ react-rps/src/components/Header.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import web3 from '../utils/web3';
 
-const Header = ({ host, bankroll }) => {
+const Header = ({ host, bankroll, fomoBalance }) => {
+  const renderBankrollText = () => {
+    if (bankroll === '0') {
+      return (
+        <p>No bankroll is available at the moment. Please wait until host deposit some bankroll to start the game.</p>
+      );
+    }
+    if (fomoBalance === '0') {
+      return <p>The current available bankroll for players to win is {web3.utils.fromWei(bankroll, 'gwei')} Gwei!</p>;
+    } else {
+      const actualAvailableBankroll = +bankroll - +fomoBalance;
+      return (
+        <p>
+          Given Fomo pool balance of {web3.utils.fromWei(String(fomoBalance), 'gwei')} Gwei, The current available
+          bankroll for players to win is {web3.utils.fromWei(String(actualAvailableBankroll), 'gwei')} Gwei
+        </p>
+      );
+    }
+  };
+
   return (
     <div>
       <h2>Rock Paper Scissors Game</h2>
       <p>This contract is hosted by {host}.</p>
-      {bankroll === '0' ? (
-        <p>No bankroll is available at the moment. Please wait until host deposit some bankroll to start the game.</p>
-      ) : (
-        <p>The current available bankroll for players to win is {web3.utils.fromWei(bankroll, 'gwei')} Gwei!</p>
-      )}
+      {renderBankrollText()}
       <hr />
     </div>
   );
