cf864305d042a82af169077d3d121c6942977ed1
gareth0712 <gareth_0712@hotmail.com>
2021-03-03 00:18:03 +0800

    Update Game component in Frontend to integrate with Fomo

--- react-rps/src/components/Game.js
+++ react-rps/src/components/Game.js
@@ -5,7 +5,7 @@ import actions from '../utils/actions';
 import randomNumber from '../utils/randomNumber';
 import Message from './Message';
 
-const Game = ({ gameMessage, setGameMessage, host, player, bankroll }) => {
+const Game = ({ gameMessage, setGameMessage, host, player, bankroll, fomoReleasingToOwner }) => {
   const [betInput, setBetInput] = useState(''); // String Since the input of user is always String
   const [playerAction, setPlayerAction] = useState('');
   const [playerBalance, setPlayerBalance] = useState('');
@@ -15,30 +15,54 @@ const Game = ({ gameMessage, setGameMessage, host, player, bankroll }) => {
     event.preventDefault();
     let betInWei;
     if (betInput !== '') {
-      betInWei = web3.utils.toWei(betInput, 'gwei');
+      betInWei = +web3.utils.toWei(betInput, 'gwei');
     }
-    if (playerAction !== '' && betInput !== '' && betInWei > 0 && betInWei <= bankroll) {
+    if (playerAction !== '' && betInput !== '' && betInWei > 0 && betInWei <= bankroll && !fomoReleasingToOwner) {
       setGameMessage('Pending transaction to be completed...');
-      await rps.methods.playerPlaceBet(web3.utils.keccak256(playerAction), randomNumber()).send({
-        from: player,
-        value: betInWei,
-      });
-
-      const winner = await rps.methods.lastWinner().call();
-      const hostLastAction = actions[await rps.methods.hostLastAction().call()];
-      if (winner === host) {
-        setGameMessage(`Host's action is ${hostLastAction}. Sorry, you lost the game. But you will win again!`);
-      } else if (winner === player) {
-        setGameMessage(
-          `Host's action is ${hostLastAction}. Congratulations, you won the game and you got ${betInput * 2} Gwei!`
-        );
-      } else {
-        setGameMessage(`Host's action is ${hostLastAction}. Draw game!`);
-      }
+      await handlePlacingBet(betInWei);
+    } else if (fomoReleasingToOwner) {
+      setGameMessage(
+        'Fomo Pool timer is over and currently pending fomo pool owner to collect the fomo rewards. Will resume the game upon user collected the rewards'
+      );
     } else {
       setGameMessage(
         'Bet amount of at least 1 Gwei and at most same amount as the bankroll will be accepted and please ensure you have selected an action in order to play the game!'
       );
+    }
+  };
+
+  const handlePlacingBet = async (betInWei) => {
+    try {
+      await rps.methods.playerPlaceBet(web3.utils.keccak256(playerAction), randomNumber()).send({
+        from: player,
+        value: betInWei,
+      });
+      const winner = await rps.methods.lastWinner().call();
+      const hostLastAction = actions[await rps.methods.hostLastAction().call()];
+      printGameMessage(winner, hostLastAction);
+    } catch (err) {
+      if (err.message.includes('User denied transaction signature')) {
+        setGameMessage(
+          'Player denied the transaction. Please note that player has to press confirm in Metamask in order to place a bet.'
+        );
+      } else {
+        setGameMessage('Error occurred in transaction. Please try again.');
+      }
+    }
+  };
+
+  const printGameMessage = (winner, hostLastAction) => {
+    if (winner === host) {
+      setGameMessage(`Host's action is ${hostLastAction}. Sorry, you lost the game. But you will win again!`);
+    } else if (winner === player) {
+      const reward = betInput * 2;
+      setGameMessage(
+        `Host's action is ${hostLastAction}. Congratulations, you won the game and the reward is ${reward}. You got ${
+          reward * 0.95
+        } Gwei! The remaining ${reward * 0.05} Gwei will be allocated to the Fomo Pool.`
+      );
+    } else {
+      setGameMessage(`Host's action is ${hostLastAction}. Draw game!`);
     }
   };
 
@@ -60,8 +84,16 @@ const Game = ({ gameMessage, setGameMessage, host, player, bankroll }) => {
         <p>Your Balance: {web3.utils.fromWei(playerBalance, 'gwei')} Gwei</p>
         <div>
           <label>Amount of Gwei to place a bet </label>
-          <input value={betInput} onChange={(event) => setBetInput(event.target.value.replace(/\D/, ''))} />
-          <select value={playerAction} onChange={(event) => setPlayerAction(event.target.value)}>
+          <input
+            disabled={bankroll === '0'}
+            value={betInput}
+            onChange={(event) => setBetInput(event.target.value.replace(/\D/, ''))}
+          />
+          <select
+            disabled={bankroll === '0'}
+            value={playerAction}
+            onChange={(event) => setPlayerAction(event.target.value)}
+          >
             <option value="" defaultValue={true}>
               Select
             </option>
