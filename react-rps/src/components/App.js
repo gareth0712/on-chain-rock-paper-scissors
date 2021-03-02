0523935c5bad2c6774d088fc8c9b789896d66c7c
gareth0712 <gareth_0712@hotmail.com>
2021-03-03 00:20:52 +0800

    Update App component to integrate with Fomo Component

--- react-rps/src/components/App.js
+++ react-rps/src/components/App.js
@@ -6,16 +6,25 @@ import rps from '../utils/rps';
 import Header from './Header';
 import Verify from './Verify';
 import Game from './Game';
+import Fomo from './Fomo';
 import HostOperations from './HostOperations';
 
 const App = () => {
   // Host-related
-  const [bankroll, setBankroll] = useState(''); // String since web3.eth.getBalance returns string
+  // String since web3.eth.getBalance returns string
+  const [bankroll, setBankroll] = useState('');
   // Application-related
   const [host, setHost] = useState('');
   const [player, setPlayer] = useState('');
   const [hostOpsMessage, setHostOpsMessage] = useState('');
   const [gameMessage, setGameMessage] = useState('');
+  // Fomo
+  const [fomoTimerOn, setFomoTimerOn] = useState(false);
+  const [fomoEndTime, setFomoEndTime] = useState(0);
+  const [fomoOwner, setFomoOwner] = useState('');
+  const [fomoBalance, setFomoBalance] = useState(0);
+  // If set to true, players are unable to place another bet until fomo owner receives his rewards
+  const [fomoReleasingToOwner, setFomoReleasingToOwner] = useState(false);
 
   const getStates = async () => {
     setHost(await rps.methods.host().call());
@@ -28,16 +37,43 @@ const App = () => {
     }
   };
 
+  // To detect changes when user change his Metamask account
+  window.ethereum.on('accountsChanged', function (accounts) {
+    getStates();
+  });
+
   useEffect(() => {
     getStates();
   });
 
   return (
     <div>
-      <Header host={host} bankroll={bankroll} />
-      <Game gameMessage={gameMessage} setGameMessage={setGameMessage} host={host} player={player} bankroll={bankroll} />
+      <Header host={host} bankroll={bankroll} fomoBalance={fomoBalance} />
+      <Fomo
+        player={player}
+        fomoTimerOn={fomoTimerOn}
+        setFomoTimerOn={setFomoTimerOn}
+        fomoEndTime={fomoEndTime}
+        setFomoEndTime={setFomoEndTime}
+        fomoOwner={fomoOwner}
+        setFomoOwner={setFomoOwner}
+        fomoBalance={fomoBalance}
+        setFomoBalance={setFomoBalance}
+        fomoReleasingToOwner={fomoReleasingToOwner}
+        setFomoReleasingToOwner={setFomoReleasingToOwner}
+      />
+      <Game
+        gameMessage={gameMessage}
+        setGameMessage={setGameMessage}
+        host={host}
+        player={player}
+        bankroll={bankroll}
+        fomoReleasingToOwner={fomoReleasingToOwner}
+      />
       <Verify />
       <HostOperations
+        bankroll={bankroll}
+        fomoBalance={fomoBalance}
         hostOpsMessage={hostOpsMessage}
         setHostOpsMessage={setHostOpsMessage}
         host={host}
