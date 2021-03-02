baad8f48bbf86a879179ae9a9600619dd73bdb7e
gareth0712 <gareth_0712@hotmail.com>
2021-03-03 00:20:03 +0800

    Update rps.js to load different on-chain rock-paper-scissors application when diffferent environment variables are given

--- react-rps/src/utils/rps.js
+++ react-rps/src/utils/rps.js
@@ -1,7 +1,19 @@
 import web3 from './web3';
 
-const address = process.env.REACT_APP_BLOCK_ADDRESS;
-const abi = JSON.parse(process.env.REACT_APP_ABI);
+let address, abi;
+
+if (process.env.REACT_APP_ENV === 'RpsAdvanced') {
+  address = process.env.REACT_APP_ADVANCED_BLOCK_ADDRESS;
+  abi = JSON.parse(process.env.REACT_APP_ADVANCED_ABI);
+} else if (process.env.REACT_APP_ENV === 'RpsAdvancedTest') {
+  address = process.env.REACT_APP_ADVANCED_TEST_BLOCK_ADDRESS;
+  abi = JSON.parse(process.env.REACT_APP_ADVANCED_TEST_ABI);
+}
+// Level 1 contract Won't work since the updated components in React requires components from level2 contract (e.g. fomo variables)
+// else if (process.env.REACT_APP_ENV === 'Rps') {
+//   address = process.env.REACT_APP_BLOCK_ADDRESS;
+//   abi = JSON.parse(process.env.REACT_APP_ABI);
+// }
 
 // The purpose of this file is to create a local instance of the contract.
 // We're going to make an object that exists only inside of our browser.
