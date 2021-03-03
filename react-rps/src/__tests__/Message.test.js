c781e7386a14ad7230d3bf6a28c8c893e82b69bd
gareth0712 <gareth_0712@hotmail.com>
2021-03-03 14:16:02 +0800

    Added Frontend test cases

---
+++ react-rps/src/__tests__/Message.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Message from '../components/Message';
+
+const fomoMessage = "Fomo Pool timer is over and currently pending fomo pool owner to collect the fomo rewards. Will resume the game upon user collected the rewards"
+
+beforeEach(() => {
+  render(<Message message={fomoMessage} />);
+})
+
+test('The heading component is visible', () => {
+  const linkElement = screen.getByRole('heading');
+  expect(linkElement).toBeVisible();
+});
+
+test('Render message in H2 header as expected', () => {
+  const linkElement = screen.getByRole('heading');
+  expect(linkElement.tagName).toEqual('H2')
+});
+
+test('Rendered message element is not empty', () => {
+  const linkElement = screen.getByRole('heading');
+  expect(linkElement).not.toBeEmptyDOMElement();
+});
+
+test('Render message as expected', () => {
+  const linkElement = screen.getByRole('heading');
+  expect(linkElement).toContainHTML(`<h2>${fomoMessage}</h2>`);
+});
---
+++ react-rps/src/__tests__/RpsForm.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import RpsForm from '../components/RpsForm';
+
+// Host is player2
+const host = "0x68b7535dc90C13054073cF05f0422B7758985341"
+const player1 = "0x21a0409e66bC90a8e77D5Fbc629Cad5A114E317e"
+const player2 = "0x68b7535dc90C13054073cF05f0422B7758985341"
+
+test('renders Heading in RpsForm', () => {
+  render(<RpsForm />);
+  const linkElement = screen.getByRole('heading');
+  expect(linkElement).toBeVisible();
+});
+
+test('Textbox is visible', () => {
+  render(<RpsForm />);
+  const linkElement = screen.getByRole('textbox');
+  expect(linkElement).toBeVisible();
+});
+
+test('Button is enabled by default', () => {
+  render(<RpsForm />);
+  const linkElement = screen.getByRole('button');
+  expect(linkElement).toBeEnabled();
+});
+
+test('Button is disabled when it is hostOnly and player is not a host', () => {
+  render(<RpsForm hostOnly={true} player={player1} host={host} />);
+  const linkElement = screen.getByRole('button');
+  expect(linkElement).not.toBeEnabled();
+});
+
+test('Button is enabled when it is hostOnly and player is a host', () => {
+  render(<RpsForm hostOnly={true} player={player2} host={host} />);
+  const linkElement = screen.getByRole('button');
+  expect(linkElement).toBeEnabled();
+});
+
+test('Display header as expected', () => {
+  const header = "Add Bankroll"
+  render(<RpsForm header={header} />);
+  const linkElement = screen.getByRole('heading');
+  expect(linkElement).toContainHTML(`<h4>${header}</h4>`);
+});
